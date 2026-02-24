45414f0561ec93990211382efaa0d0a3dfcf37b9
shivanis-rao <shivani.s@nineleaps.com>
2026-02-24 15:37:15 +0530

    made changes

--- react/src/services/UserServices.ts
+++ react/src/services/UserServices.ts
@@ -1,7 +1,7 @@
 import ApiService from "./ApiService"
 
 const login = (email:string,password:string)=>{
-    apiKey= "AIzaSyC0vdHUxgPZ9KByiA6rft4QJR8FHRe1Urw"
+    const apiKey= "AIzaSyC0vdHUxgPZ9KByiA6rft4QJR8FHRe1Urw"
     const url ="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key="+apiKey;
     const data ={email,password,returnSecureToken:true};
     return ApiService.post(url,data)
